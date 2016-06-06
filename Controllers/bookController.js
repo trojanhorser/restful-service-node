431acae3a36f8044bcc9816ee6c62bdcb83d4ea1
trojanhorser <denisdbell@gmail.com>
2016-06-06 17:22:47 -0500

    moved all logic to the controller

--- Controllers/bookController.js
+++ Controllers/bookController.js
@@ -24,11 +24,82 @@ var bookController = function(Book){
           }
       });
   }
+  
+  var bookRouterMiddleWear = function(req,res,next){
+   Book.findById(req.params.bookId, function(err,book){
+       if(err){
+           res.status(500).send(err);
+       }else if(book){
+           req.book = book;
+           next();
+       }else{
+           res.status(404).send('no book found');
+       }
+   }) 
+}
+
+var getById = function(req,res){      
+      res.json(req.book);
+}
+
+var update = function(req,res){
+    req.book.title = req.body.title;
+    req.book.author = req.body.autor;
+    req.book.genre = req.body.genre;
+    req.book.read = req.body.read;
+    
+    req.book.save(function(err){
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.json(req.book);
+        }            
+    });
+            
+  }
+
+var patch = function(req,res){
+      
+      if(req.body._id){
+          delete req.body._id;
+      }
+      
+      for(var key in req.body){
+          
+          req.book[key] = req.body[key];
+      }
+        
+      req.book.save(function(err){
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.json(req.book);
+        }            
+      });      
+     
+  }
+ 
+var removeBook = function(req,res){
+      req.book.remove(function(err){
+          if(err){
+              res.status(500).send(err);
+          }else{
+              res.status(204).send('Removed');
+          }
+      });
+  }
+
+
    
   return {
       post: post,
-      get: get
-  }  
+      get: get,
+      middleWear: bookRouterMiddleWear,
+      getById: getById,
+      update: update,
+      patch: patch,
+      removeBook: removeBook
+    }  
 }
 
 module.exports = bookController;
--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -2,78 +2,22 @@ var express = require('express');
 
 var bookRouter = express.Router(); 
 
-var bookController = require('../Controllers/bookController')(Book);
 
 var routes = function(Book){
- 
-console.log("bookController " +bookController);
 
-/*bookRouter.route('/')
+var bookController = require('../Controllers/bookController')(Book);
+
+bookRouter.route('/')
   .post(bookController.post)
   .get(bookController.get);
-*/
-bookRouter.use('/:bookId',function(req,res,next){
-   Book.findById(req.params.bookId, function(err,book){
-       if(err){
-           res.status(500).send(err);
-       }else if(book){
-           req.book = book;
-           next();
-       }else{
-           res.status(404).send('no book found');
-       }
-   }) 
-});
+
+bookRouter.use('/:bookId',bookController.middleWear);
 
 bookRouter.route('/:bookId')
-  .get(function(req,res){
-           
-      res.json(req.book);
-  })
-  .put(function(req,res){
-    book.title = req.body.title;
-    book.author = req.body.autor;
-    book.genre = req.body.genre;
-    book.read = req.body.read;
-    
-    req.book.save(function(err){
-        if(err){
-            res.status(500).send(err);
-        }else{
-            res.json(req.book);
-        }            
-    });
-            
-  })
-  .patch(function(req,res){
-      
-      if(req.body._id){
-          delete req.body._id;
-      }
-      
-      for(var key in req.body){
-          
-          req.book[key] = req.body[key];
-      }
-        
-      req.book.save(function(err){
-        if(err){
-            res.status(500).send(err);
-        }else{
-            res.json(req.book);
-        }            
-      });      
-     
-  })
-  .delete(function(req,res){
-      req.book.remove(function(err){
-          if(err){
-              res.status(500).send(err);
-          }else{
-              res.status(204).send('Removed');
-          }
-      });
-  });
+  .get(bookController.getById)
+  .put(bookController.update)
+  .patch(bookController.patch)
+  .delete(bookController.removeBook);
 return bookRouter;
 };
 
